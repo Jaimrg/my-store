eaa7f23fead4916d6c2f195b1b9675e22599d84b
Jaime RG <75501289+Jaimrg@users.noreply.github.com>
2021-11-04 06:03:22 +0200

    products atl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,28 @@ import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, MatToolbarModule, MatIconModule, BrowserAnimationsModule,MatButtonModule],
+  declarations: [AppComponent,ProductListComponent],
+  imports: [BrowserModule, MatToolbarModule, MatIconModule, BrowserAnimationsModule,MatButtonModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent }])
+  
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+function providers(arg0: { path: string; component: typeof ProductListComponent; }[], providers: any, arg2: never[], bootstrap: any, arg4: (typeof AppComponent)[]): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
+  throw new Error('Function not implemented.');
+}
+function bootstrap(arg0: { path: string; component: typeof ProductListComponent; }[], providers: (arg0: { path: string; component: typeof ProductListComponent; }[], providers: any, arg2: never[], bootstrap: any, arg4: (typeof AppComponent)[]) => any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}>, arg2: never[], bootstrap: any, arg4: (typeof AppComponent)[]): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
+  throw new Error('Function not implemented.');
+}
+
